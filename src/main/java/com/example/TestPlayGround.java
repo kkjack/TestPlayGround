cb5aa6084c6400b9b02b3dff99d17a155b086a06
kkjacks <kkjacks@gmail.com>
2016-07-24 19:35:30 -0400

    Init

---
+++ src/main/java/com/example/TestPlayGround.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPlayGround {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPlayGround.class, args);
+    }
+}
---
+++ src/main/java/com/example/repo/User.java
@@ -0,0 +1,56 @@
+package com.example.repo;
+
+import javax.persistence.Entity;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    private UUID uuid = null; //UUID.randomUUID();
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+
+    private Integer zipCode;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+}
